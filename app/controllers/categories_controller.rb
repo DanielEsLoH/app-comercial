201b54c97d4976d41d81d0a32b3a198f2951291b
Daniel Londoño Henao <daniel.esloh@gmail.com>
2023-02-10 00:09:17 -0500

    CRUD modelo categorias

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,62 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[edit update destroy]
+  def index
+    @categorias = Category.all
+  end
+
+  def new
+    @categoria = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    @categoria = Category.new(category_params)
+    respond_to do |format|
+      if @categoria.save
+        format.json { head :no_content }
+        format.js
+      else
+        format.json do
+          render json: @categoria.errors.full_messages,
+                 status: :unprocessable_entity
+        end
+        format.js { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @categoria.update(category_params)
+        format.json { head :no_content }
+        format.js
+      else
+        format.json do
+          render json: @categoria.errors.full_messages,
+                 status: :unprocessable_entity
+        end
+        format.js { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @categoria.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+      format.js
+    end
+  end
+
+  private
+
+  def set_category
+    @categoria = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:nombre, :descripcion)
+  end
+end
